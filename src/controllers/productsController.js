8fc2ab1b435ac505b899c7c0aec9b4aa3798505a
MVBarrionuevo <vikibarrionuevo@gmail.com>
2021-11-04 20:52:53 -0300

    modifique data user + cree pantalla abmDetalle

--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,4 +1,4 @@
-const path = require ('path');
+const path = require('path');
 const fs = require('fs')
 
 const productsFilePath = path.join(__dirname, '../../data/products.json');
@@ -8,56 +8,61 @@ const productController = {
     productos: (req, res) => {
         // le pasamos a la vista el array de productos que obtuvimos a partir
         // del JSON.
-        res.render('productos', {productos:products})
+        res.render('productos', { productos: products })
     },
     detalle: (req, res) => { //muestra detalle product
         const id = req.params.id;
         const producto = products.find(product => {
-            return product.id == id; 
-		})
-        res.render('detalleProducto' ,{productSend:producto})
+            return product.id == id;
+        })
+        res.render('detalleProducto', { productSend: producto })
     },
     carrito: (req, res) => {
-        res.render ('carritoProductos')
+        res.render('carritoProductos')
     },
     abmCrear: (req, res) => // muestra pantalla crear
-        {res.render ('abmCrear')
+    {
+        res.render('abmCrear')
     },
-    abmCreado: (req,res) =>{ // accion de agregar prod   
-          let newProduct = {
-			id: 22222, //products[products.length -1].id + 1,
+    abmCreado: (req, res) => { // accion de agregar prod   
+        let newProduct = {
+            id: 22222, //products[products.length -1].id + 1,
             name: req.body.nombre,
             description: req.body.descripcion,
             price: req.body.precio,
-            image: "/img/aro2.jpg" , //-------PENDIENTE--------
-            category: req.body.categoria ,
-		}
-            products.push(newProduct);
-            fs.writeFileSync(productsFilePath, JSON.stringify(products,null," "))
-          res.render('abmListar')
-    }, 
-    abmListar: (req, res) => { 
-        res.render('abmListar', {productos:products})
+            image: "/img/aro2.jpg", //-------PENDIENTE--------
+            category: req.body.categoria,
+        }
+        products.push(newProduct);
+        fs.writeFileSync(productsFilePath, JSON.stringify(products, null, " "))
+        res.render('abmListar')
+    },
+    abmListar: (req, res) => {
+        res.render('abmListar', { productos: products })
     },
     abmEditar: (req, res) => {
         const id = req.params.id;
         const producto = products.find(product => {
-            return product.id == id; 
-		})
-        res.render('abmEditar', {productoAEditar:producto});
+            return product.id == id;
+        })
+        res.render('abmEditar', { productoAEditar: producto });
     },
-    abmEliminar:  (req, res) => {
+    abmEliminar: (req, res) => {
         // metodo de eliminacion de prod
     },
 
-    abmDetalle:(req, res) => {
-        res.render ('abmCrear')//cambiar la url, la puse para q no se rompa!
+    abmDetalle: (req, res) => {
+        const id = req.params.id;
+        const producto = products.find(product => {
+            return product.id == id;
+        })
+        res.render('abmDetalle', { detalleDeProducto: producto });
     },
 
-    abmEditado:(req,res) => {
+    abmEditado: (req, res) => {
 
     }
-    
+
 }
 
 module.exports = productController;
