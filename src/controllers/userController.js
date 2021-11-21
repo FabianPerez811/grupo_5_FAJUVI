cb8405082369de94ee1881f8befaea7b5b53b719
FabianPerez811 <borelloperez@gmail.com>
2021-11-21 19:21:59 -0300

    Proceso registro

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -2,9 +2,8 @@ const path = require ('path');
 const fs = require('fs')
 const User = require('../../Models/Users.js')
 
-const productsFilePath = path.join(__dirname, '../../data/products.json');
-const products = JSON.parse(fs.readFileSync(productsFilePath, 'utf-8'));
-  
+const usersFilePath = path.join(__dirname, '../../data/users.json');
+const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));
 
 const userController = {
 
@@ -12,8 +11,20 @@ registro: (req, res) => {
     res.render ('registro') 
 }, 
 
-procesoRegistro: (req, res) => {
-    return res.send (req.body)
+procesoRegistro: (req, res) => { //crear usuario
+    let newUser = {
+        id: User.generarID(),
+        firstName: req.body.nombre,
+        lastName: req.body.apellido,
+        email: req.body.email,
+        password: req.body.password,
+        category: req.body.catUsuario,
+        image: null
+    }
+    users.push(newUser);
+    fs.writeFileSync(usersFilePath, JSON.stringify(users, null, " "))
+    res.render('acceso')
+
 },
 
 acceso:(req, res) => {
